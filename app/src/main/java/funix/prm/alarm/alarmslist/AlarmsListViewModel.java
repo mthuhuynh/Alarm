c24f8f50d51b232adb4b6b24424454e677347f3c
mthuhuynh <61845626+mthuhuynh@users.noreply.github.com>
2021-05-04 02:06:33 +0700

    Initial commit

---
+++ app/src/main/java/funix/prm/alarm/alarmslist/AlarmsListViewModel.java
@@ -0,0 +1,32 @@
+package funix.prm.alarm.alarmslist;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import funix.prm.alarm.data.Alarm;
+import funix.prm.alarm.data.AlarmRepository;
+
+public class AlarmsListViewModel extends AndroidViewModel {
+    private AlarmRepository alarmRepository;
+    private LiveData<List<Alarm>> alarmsLiveData;
+
+    public AlarmsListViewModel(@NonNull Application application) {
+        super(application);
+
+        alarmRepository = new AlarmRepository(application);
+        alarmsLiveData = alarmRepository.getAlarmsLiveData();
+    }
+
+    public void update(Alarm alarm) {
+        alarmRepository.update(alarm);
+    }
+
+    public LiveData<List<Alarm>> getAlarmsLiveData() {
+        return alarmsLiveData;
+    }
+}
---
+++ app/src/main/java/funix/prm/alarm/alarmslist/OnToggleAlarmListener.java
@@ -0,0 +1,7 @@
+package funix.prm.alarm.alarmslist;
+
+import funix.prm.alarm.data.Alarm;
+
+public interface OnToggleAlarmListener {
+    void onToggle(Alarm alarm);
+}
