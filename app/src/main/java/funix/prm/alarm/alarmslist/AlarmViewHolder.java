21c61704713365627bbdf756f2c682befe4874ea
mthuhuynh <61845626+mthuhuynh@users.noreply.github.com>
2021-05-04 04:22:09 +0700

    ActivityMain - AddButton ListAlarm - item_alarm

--- app/src/main/java/funix/prm/alarm/alarmslist/AlarmViewHolder.java
+++ app/src/main/java/funix/prm/alarm/alarmslist/AlarmViewHolder.java
@@ -49,9 +49,9 @@ public class AlarmViewHolder extends RecyclerView.ViewHolder {
         }
 
         if (alarm.getTitle().length() != 0) {
-            alarmTitle.setText(String.format("%s | %d | %d", alarm.getTitle(), alarm.getAlarmId(), alarm.getCreated()));
+            alarmTitle.setText(alarm.getTitle());
         } else {
-            alarmTitle.setText(String.format("%s | %d | %d", "Alarm", alarm.getAlarmId(), alarm.getCreated()));
+            alarmTitle.setText("Alarm");
         }
 
         alarmStarted.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
